3eb547e5fe9c1e0429899c86c7387c9ab90338d0
santosh <santosh@acalvio.com>
2020-05-12 17:50:56 +0530

    Fixed malloc address error by using size(struct) to allocate memory using malloc instead of size((struct*)0), not sure about the difference

--- DB/src/main.c
+++ DB/src/main.c
@@ -60,7 +60,6 @@ const int id_offset = 0;
 const int name_offset = id_offset + ID_SIZE;
 const int email_offset = name_offset + NAME_SIZE;
 
-
 typedef struct Pager
 {
     void* page;
@@ -87,10 +86,34 @@ const uint32_t PAGE_NUMBER_OFFSET = PARENT_POINTER_OFFSET + PARENT_POINTER_SIZE;
 const uint32_t SIZE_HEADER = NODE_TYPE_SIZE + IS_ROOT_SIZE + NUM_OF_ROWS_SIZE + PARENT_POINTER_OFFSET + PAGE_NUMBER_SIZE;
 const uint32_t DATA_OFFSET = SIZE_HEADER;
 
+const short max_number_of_rows_in_a_page = (PAGE_SIZE - DATA_OFFSET)/ROW_SIZE;
+
+// Number of keys and their corresponding pointers that can be stored in an internal node.
+// ID_SIZE is the size of key. PAGE_NUMBER_SIZE is the size of the page number.
+// Subtracting one because, number of pointers/(page numbers) is one greater than number of keys.
+int max_number_of_keys = (PAGE_SIZE - DATA_OFFSET)/(ID_SIZE + PAGE_NUMBER_SIZE) - 1;
+
+// Following function defines the structure of internal node.
+// Internal node doesn't store data. It stores meta data about node and keys and page numbers of children.
+// Not limiting number of keys that can be stored in an internal node. Storing as many as we can accommodate in a page.
+Pager* initialise_internalnode(){
+    Pager* page = malloc(sizeof((Pager*)0));
+    page->page_number = -1;
+    page->page = malloc(PAGE_SIZE);
+    page->node_type = NODE_INTERNAL;
+    page->is_root = 0;
+    // This stores the number of keys present in the an internal node.
+    page->num_of_rows = 0;
+    page->parent_pagenumber = -1;
+    return page;
+}
+
 
 typedef struct{
     short root_page_number;
     short number_of_rows_in_table;
+    // Add file reading code for this.
+    short number_of_pages;
     Pager* pager;
 } Table;
 
@@ -98,11 +121,11 @@ typedef struct
 {
     short row_number;
     short page_number;
-    Table* table;
+    // Table* table;
 } Cursor;
 
 Pager* initialise_pager(){
-    Pager* page = malloc(sizeof((Pager*)0));
+    Pager* page = malloc(sizeof(Pager));
     page->page_number = -1;
     page->page = malloc(PAGE_SIZE);
     page->node_type = NODE_INTERNAL;
@@ -117,13 +140,12 @@ void free_object(void* object){
 }
 
 Row* get_row(){
-    Row* row = NULL;
-    row = malloc(sizeof(Row));
+    Row* row = malloc(sizeof(Row));
     return row;
 }
 
 Table* initialise_table(){
-    Table* table = malloc(sizeof((Table*)0));
+    Table* table = malloc(sizeof(Table));
     table->root_page_number = -1;
     table->number_of_rows_in_table = 0;
     table->pager = initialise_pager();
@@ -186,8 +208,8 @@ Pager* loadpage(short page_number, FILE* fptr){
 
 Cursor* table_start(Table* table){
     // This function initialises cursor to point to the root of the table.
-    Cursor* cursor = malloc(sizeof((Cursor*)0));
-    cursor->table = table;
+    Cursor* cursor = malloc(sizeof(Cursor));
+    // cursor->table = table;
     // Load the root page.
     cursor->page_number = table->root_page_number;
     cursor->row_number = 0;
@@ -222,41 +244,44 @@ void write_table_data_to_file(Table* table, FILE* fptr){
     }
 }
 
-void* cursor_value(Cursor* cursor, FILE* fptr){
+void* cursor_value(Cursor* cursor, Table* table, FILE* fptr){
 
     // Checking if page is already loaded.
-    if(cursor->page_number != cursor->table->pager->page_number){
-        cursor->table->pager = loadpage(cursor->page_number, fptr);
+    if(cursor->page_number != table->pager->page_number){
+        // free_object(table->pager);
+        table->pager = loadpage(cursor->page_number, fptr);
     }
 
     // Checking if maximum number of rows are inserted already
     int max_number_of_rows_in_a_page = (PAGE_SIZE - DATA_OFFSET)/ROW_SIZE;
     if (cursor->row_number >= max_number_of_rows_in_a_page){
         printf("Maximum number of rows have been inserted\n");
-        write_table_data_to_file(cursor->table, fptr);
+        write_table_data_to_file(table, fptr);
         exit(EXIT_FAILURE);
     }
 
     printf("%s%d\n", "rownumber:", cursor->row_number);
     uint32_t row_number_address = (cursor->row_number * ROW_SIZE);
-    printf("%s:%p", "Page Address", cursor->table->pager->page);
+    printf("%s:%p", "Page Address", table->pager->page);
     printf("%s:%d", "Data Offset", DATA_OFFSET);
     printf("%s:%d", "Row Number Address", row_number_address);
-    printf("%s:%p", "Cursor Value", cursor->table->pager->page + DATA_OFFSET + row_number_address);
-    return cursor->table->pager->page + DATA_OFFSET + row_number_address;
+    printf("%s:%p", "Cursor Value", table->pager->page + DATA_OFFSET + row_number_address);
+    return table->pager->page + DATA_OFFSET + row_number_address;
 }
 
 Cursor* table_iterator(int key, Table* table, FILE* fptr){
     // This function iterates through the table and finds the correct position where key should be inserted and
     // points the cursor accordingly.
-    Cursor* cursor = malloc(sizeof((Cursor*)0));
-    cursor->table = table;
+    Cursor* cursor = malloc(sizeof(Cursor));
+    // cursor->table = table;
     cursor->page_number = table->root_page_number;
-    if(cursor->page_number != cursor->table->pager->page_number){
-        cursor->table->pager = loadpage(cursor->page_number, fptr);
+    if(cursor->page_number != table->pager->page_number){
+        // Freeing the previously allocated page.
+        // free_object(table->pager);
+        table->pager = loadpage(cursor->page_number, fptr);
     }
     Row* row = get_row();
-    short max_index_row = (cursor->table->pager->num_of_rows - 1);
+    short max_index_row = (table->pager->num_of_rows - 1);
     short min_row_number = 0;
     short max_row_number = max_index_row;
 
@@ -266,7 +291,7 @@ Cursor* table_iterator(int key, Table* table, FILE* fptr){
     // it finds the number which is just higher that the key value.
     while (min_row_number < max_row_number){
         mid = floor((min_row_number + max_row_number)/2);
-        desrialise(cursor->table->pager->page + DATA_OFFSET + (mid * ROW_SIZE), row);
+        desrialise(table->pager->page + DATA_OFFSET + (mid * ROW_SIZE), row);
         if (key > row->id){
             min_row_number = mid + 1;
         }
@@ -276,20 +301,20 @@ Cursor* table_iterator(int key, Table* table, FILE* fptr){
         else{
             // Checking if key is already present.
             printf("Duplicate Primary Key Error\n");
-            write_table_data_to_file(cursor->table, fptr);
+            write_table_data_to_file(table, fptr);
             exit(EXIT_FAILURE);
         }
     }
 
     // Checking if there are no rows present.
     if (max_row_number == -1){
-        free_object(row);
+        free(row);
         cursor->row_number = 0;
         return cursor;
     }
 
     // Checking if key is greater than last element also.
-    desrialise(cursor->table->pager->page + DATA_OFFSET + (max_index_row * ROW_SIZE), row);
+    desrialise(table->pager->page + DATA_OFFSET + (max_index_row * ROW_SIZE), row);
 
     if (key > row->id){
         // Making max_row_number point next index.
@@ -299,21 +324,127 @@ Cursor* table_iterator(int key, Table* table, FILE* fptr){
     // Checking if key is same as last element.
     if (key == row->id){
         printf("Duplicate Primary Key Error\n");
-        write_table_data_to_file(cursor->table, fptr);
-        free_object(row);
+        write_table_data_to_file(table, fptr);
+        free(row);
         exit(EXIT_FAILURE);
     }
 
     // Moving the data to make space for new entry.
     if (max_row_number <=  max_index_row){
         short num_rows_to_be_copied = (max_index_row - max_row_number + 1);
-        memcpy(cursor->table->pager->page + DATA_OFFSET + ((max_row_number+1) * ROW_SIZE), cursor->table->pager->page + DATA_OFFSET + (max_row_number * ROW_SIZE), (num_rows_to_be_copied * ROW_SIZE));
+        memcpy(table->pager->page + DATA_OFFSET + ((max_row_number+1) * ROW_SIZE), table->pager->page + DATA_OFFSET + (max_row_number * ROW_SIZE), (num_rows_to_be_copied * ROW_SIZE));
     }
 
-    free_object(row);
+    free(row);
     cursor->row_number = max_row_number;
     return cursor;
 }
+
+uint32_t get_key_at_a_location(short location, Pager* pager){
+    /*
+     * This function reads reads the key present at a given location in a page.
+     */
+    uint32_t key;
+    memcpy(&key, pager->page + DATA_OFFSET + (location * ID_SIZE), ID_SIZE);
+    return key;
+}
+
+short binary_search_internalnode(int key, Pager* pager){
+    /*
+     * This function searches the internal node to find which children will contain the row with key "key".
+     * This function returns the index of keys which is next greatest element than key.
+     * Here we assume that all nodes less than or equal to a key goes to left and rest to right(BST Analogy)
+     */
+    short last_key_index = pager->num_of_rows - 1;
+    short start_key_index = 0;
+    short mid = 0;
+    uint32_t mid_key;
+    while (start_key_index < last_key_index) {
+        mid = floor((last_key_index + start_key_index) / 2);
+        mid_key = get_key_at_a_location(mid, pager);
+        if (key > mid_key) {
+            start_key_index = mid + 1;
+        } else if (key < mid_key) {
+            last_key_index = mid;
+        } else {
+            return mid;
+        }
+    }
+    return last_key_index;
+}
+
+
+Cursor* traverse_tree(int key, Table* table, FILE* fptr){
+    /*
+     * This function traverses tree starting from the root node and finds the exact place the new should be
+     * inserted. It also splits the nodes if the number of rows in a node reached maximum value.
+     */
+    // Initialising a cursor which points to the root node.
+    Cursor* cursor = table_start(table);
+    // Load the root page if its not loaded.
+    if (cursor->page_number != table->pager->page_number){
+        table->pager = loadpage(cursor->page_number, fptr);
+    }
+
+    short key_index;
+    short last_key;
+    short req_pagenumber_index, req_pagenumber;
+    // Traverse through the Tree till we are in a leaf node.
+    while (table->pager->node_type != NODE_LEAF){
+        key_index = binary_search_internalnode(key, table->pager);
+        /*
+         * Its like this. List of keys are stored and list of page numbers are stored. number of pager numbers
+         * will be one greater than equal to number of keys. All keys less than or equal to key at a index will be
+         * present/should go to page number at same index. So just for the last key, if all keys greater than
+         * last key should go to page at index, index + 1.
+        */
+        req_pagenumber_index = key_index;
+        // Checking if the key lies in last or last but one page.
+        if (key_index == (table->pager->num_of_rows - 1)){
+            last_key = get_key_at_a_location(key_index, table->pager);
+            if (key > last_key){
+                req_pagenumber_index = key_index + 1;
+            }
+        }
+
+        // Load  page number which is present at req_pagenumber_index.
+        //
+
+        req_pagenumber = get_key_at_a_location(table->pager->num_of_rows + req_pagenumber_index, table->pager);
+        table->pager = loadpage(req_pagenumber, fptr);
+    }
+
+    // After while loop we get a leaf node where the given key can be inserted.
+
+    /*
+     * Now we have to search the leaf node to insert this key.
+     * Following are scenarios.
+     * Node can accommodate one more row.
+     * Here, we use binary search to find the position where this should be inserted. We have to move rows to create
+     * space for the new row.
+     * If there are already maximum number of rows in the node, then we have to split this node and move one key to
+     * parent. Same check has to be done till the root node.
+    */
+    // Checking if we have to split the node.
+    if (table->pager->num_of_rows == max_number_of_rows_in_a_page){
+        Pager* pager = initialise_pager();
+        pager->parent_pagenumber = table->pager->parent_pagenumber;
+        pager->node_type = NODE_LEAF;
+        // Check this.
+        pager->is_root = 0;
+        pager->num_of_rows = floor(table->pager->num_of_rows/2);
+        pager->page_number = table->number_of_pages + 1;
+        table->number_of_pages += 1;
+        memcpy(pager->page + DATA_OFFSET, table->pager->page + DATA_OFFSET + (
+                (table->pager->num_of_rows - pager->num_of_rows) * ROW_SIZE), pager->num_of_rows * ROW_SIZE);
+        // Insert the splitting key in the parent node.
+        
+
+    }
+    // Binary seach on the final node to get the position where new node should be inserted.
+    return cursor;
+}
+
 
 InputBuffer* get_inputbuffer(){
     InputBuffer* input_buffer = NULL;
@@ -358,14 +489,14 @@ int insert_row_into_table(Row* row, Table* table, FILE* fptr){
         return PREPARE_STRING_TOO_LONG;
     }
     Cursor* cursor = table_iterator(row->id, table, fptr);
-    serialize(row, cursor_value(cursor, fptr));
+    serialize(row, cursor_value(cursor, table, fptr));
     table->pager->num_of_rows += 1;
     table->number_of_rows_in_table += 1;
     printf("%s:%zu\n", "num_of_rows in page", table->pager->num_of_rows);
     printf("%s:%hd\n", "Page Number", table->pager->page_number);
     printf("%s:%hd\n", "Parent Page Number", table->pager->parent_pagenumber);
     printf("%s:%d\n", "Row inserted", table->number_of_rows_in_table);
-    free_object(cursor);
+    free(cursor);
     return EXIT_SUCCESS;
 }
 
@@ -376,13 +507,28 @@ void print_all_rows(Row* row, Table* table, FILE* fptr){
     for(start_row=0; start_row < table->number_of_rows_in_table; start_row++){
         // Loading the page if its not loaded into the table yet.
         cursor->row_number = start_row;
-        desrialise(cursor_value(cursor, fptr), row);
+        desrialise(cursor_value(cursor, table, fptr), row);
         printf("%d,%s,%s\n", row->id, row->name, row->email);
     }
-    free_object(cursor);
+    free(cursor);
 }
 
 int process_sql_statements(char* statement, Table* table, FILE* fptr){
+    if (strncmp(statement, "insert_multiple", 15) == 0){
+        // Inserting multiple rows for checking size full.
+        printf("Multiple rows will be inserted\n");
+        short i = 0;
+        Row* row = get_row();
+        strcpy(row->name , "a");
+        strcpy(row->email , "b");
+
+        for (int j = 0; j < 13; ++j) {
+            row->id = j;
+            insert_row_into_table(row, table, fptr);
+        }
+        free(row);
+        return EXIT_SUCCESS;
+    }
     if (strncmp(statement, "insert", 6) == 0){
         printf("Insert statement will be processed\n");
         Row* row = get_row();
@@ -395,7 +541,7 @@ int process_sql_statements(char* statement, Table* table, FILE* fptr){
         // Insert row into the table.
         insert_row_into_table(row, table, fptr);
 
-        free_object(row);
+        free(row);
         return EXIT_SUCCESS;
 
     }
@@ -403,7 +549,7 @@ int process_sql_statements(char* statement, Table* table, FILE* fptr){
     else if(strncmp(statement, "select_all", 10) == 0){
         Row* row = get_row();
         print_all_rows(row, table, fptr);
-        free_object(row);
+        free(row);
         return EXIT_SUCCESS;
     }
 
@@ -417,7 +563,7 @@ int process_sql_statements(char* statement, Table* table, FILE* fptr){
             return EXIT_FAILURE;
         }
         // fetch_row_from_table(id, row, table);
-        free_object(row);
+        free(row);
         return EXIT_SUCCESS;
     }
     else{
@@ -499,10 +645,6 @@ int main(void)
                 continue;
             }
         }
-        free_buffer(input_buffer);
-
 
     }
-
-    return 0;
 }
